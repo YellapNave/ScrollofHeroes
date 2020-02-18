89858712471ae15c994fdb3cfa10837b8f8fa879
Evan Davis-Palley <wowccraft@gmail.com>
2020-02-17 17:00:11 -0800

    Updated hero component to accomodate lists of heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,7 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Tinker' },
+    { id: 12, name: 'Sul' },
+    { id: 13, name: 'Iba' },
+    { id: 14, name: 'Gomer' },
+    { id: 15, name: 'Jester' },
+    { id: 16, name: 'Caduceus' },
+    { id: 17, name: 'Caleb' },
+    { id: 18, name: 'Nott' },
+    { id: 19, name: 'Fjord' },
+    { id: 20, name: 'Yasha' }
+]
